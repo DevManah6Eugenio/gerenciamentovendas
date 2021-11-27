84c1e9ee091459957a22412aad38d4c476fa395d
DevManah6Eugenio <manasses.dev@gmail.com>
2021-11-27 05:41:04 -0300

    tratamento de erro se o categoria não for encontrado

--- src/main/java/com/gerenciamentovendas/services/CategoriaService.java
+++ src/main/java/com/gerenciamentovendas/services/CategoriaService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.gerenciamentovendas.domain.Categoria;
 import com.gerenciamentovendas.repositories.CategoriaRepository;
+import com.gerenciamentovendas.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -17,7 +18,7 @@ public class CategoriaService {
 	
 	public Categoria buscar(UUID id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Categoria não encontrado! Id: " + id));
 	}
 	
 	public Categoria cadastrar(Categoria categoria) {
